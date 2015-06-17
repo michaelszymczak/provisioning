f053c77e187df8e815912eb308475ade8baa6157
Michael Szymczak <contact@michaelszymczak.com>
2015-06-17 16:19:11 +0100

    Added tree package

--- shell.pp
+++ shell.pp
@@ -1,6 +1,8 @@
 $username = "maikeru"
-package { 'git': }
+package { 'git': ensure => present }
 class { 'ohmyzsh': }
 ohmyzsh::install { $username: }
 ohmyzsh::theme { $username: theme => 'ys' }
 ohmyzsh::plugins { $username: plugins => 'git github' }
+
+package { 'tree': ensure => present }
