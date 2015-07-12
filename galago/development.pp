ab996cee3b78bdaf1559b19a2960792f70390785
Michael Szymczak <contact@michaelszymczak.com>
2015-07-12 21:48:27 +0100

    Added bower as a useful JS tool. Added helper runner in the root directory

--- galago/development.pp
+++ galago/development.pp
@@ -1,7 +1,5 @@
+# JVM
 package { 'maven': ensure => present }
-package { 'mongodb': ensure => present }
-package { 'ruby': ensure => present }
-package { 'ruby-dev': ensure => present }
 package { 'gradle': ensure => present }
 
 augeas { "gradle home":
@@ -12,19 +10,33 @@ augeas { "gradle home":
   ],
 }
 
+# Other
+package { 'mongodb': ensure => present }
+package { 'ruby': ensure => present }
+package { 'ruby-dev': ensure => present }
 
+
+# Javascript
 class { '::nodejs':
   manage_package_repo       => false,
   nodejs_dev_package_ensure => 'present',
   npm_package_ensure        => 'present',
-} ->
-package { 'grunt-cli':
-  ensure   => 'present',
-  provider => 'npm',
-} ->
+}
+
 file { '/usr/bin/node':
    ensure => 'link',
    target => '/usr/bin/nodejs',
+   require => Package['npm'],
 }
 
-
+package { 'grunt-cli':
+  ensure   => 'present',
+  provider => 'npm',
+  require => Package['npm'],
+}
+ 
+package { 'bower':
+  ensure   => 'present',
+  provider => 'npm',
+  require => Package['npm'],
+}
